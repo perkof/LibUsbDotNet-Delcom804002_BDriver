0877d9dc6304d54e8e8d0b9aada3ca1f2a391e81
Fraser Perkins <Fraser.Perkins@readify.net>
2012-08-20 19:30:10 +1000

    Removing unused references in lightcolour

--- Delcom804002-BDriver/LightColour.cs
+++ Delcom804002-BDriver/LightColour.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Delcom804002_BDriver
+﻿namespace Delcom804002_BDriver
 {
     public enum LightColour
     {
